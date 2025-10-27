5156b2b28cf05a3c8a6382082cbc1e3ed4651000
li-hong-yue <lihongyuelhy@gmail.com>
2025-10-27 02:20:16 +0000

    update

--- algorithms/search.py
+++ algorithms/search.py
@@ -85,7 +85,6 @@ class MCTS():
                 self.Ps[s] /= sum_Ps_s  # normalize
             else:
                 # if all valid moves were masked make all valid moves equally probable
-
                 # NB! All valid moves may be masked if either your NNet architecture is insufficient or you've get overfitting or something else.
                 # If you have got dozens or hundreds of these messages you should pay attention to your NNet and/or training process.   
                 log.error("All valid moves were masked, doing a workaround.")
